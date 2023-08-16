ea8265ae3b4c04a53e735db50bc51e30fb5db09d
DrashtiS <94853646+DrashtiSanjayShah@users.noreply.github.com>
2023-08-16 21:21:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,47 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 const form = documnet.getElementbyId('contactFor');
 
+// Form starts here
+<form action="" class="contact-form" id="contactForm">
+                <div class="input-control i-c-2">
+                  <input type="text" id='name' required placeholder="YOUR NAME" />
+                  <input type="text" id='mail' required placeholder="YOUR EMAIL" />
+                </div>
+                <div class="input-control">
+                  <input type="text" id='subject' required placeholder="ENTER SUBJECT" />
+                </div>
+                <div class="input-control">
+                  <textarea
+                    name="text"
+                    id="message"
+                    cols="25"
+                    rows="8"
+                
+                    placeholder="WRITE YOUR MESSAGE HERE"
+                  ></textarea>
+                
+                </div>
+                <div class="submit-btn">
+                  <a href="Drashti_Shah_Resume.pdf" target="_blank" download class="main-btn">
+                    <span class="btn-text">Download CV</span>
+                    <span class="btn-icon"
+                      ><i class="fas fa-download"></i
+                    ></span>
+                  </a>
+                </div>
+              </form>
+
+// form ends here
+
 form.addEventListener('submit',()=>{
     e.preventdefault();
-    const 
-}
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('mail').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    form.reset()
+})
 
 function PageTransitions(){
     // Button click active class
